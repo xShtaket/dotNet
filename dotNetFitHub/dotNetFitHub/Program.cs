42f478de9772848ac74e7c3632f8627ea51190a8
xShtaket <ivakhiv.vo@gmail.com>
2017-01-19 17:10:28 +0200

    add !

--- dotNetFitHub/dotNetFitHub/Program.cs
+++ dotNetFitHub/dotNetFitHub/Program.cs
@@ -10,7 +10,7 @@ namespace dotNetFitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!!");
             Console.ReadKey();
         }
     }
